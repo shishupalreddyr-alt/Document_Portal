e75e02c2317630998ff44fb0763fad440e249bbf
Shishupalreddyr-alt <rshishupalr@gmail.com>
2026-02-21 20:06:01 +0530

    SingleDoc chat Code updated

--- src/singledocument_chat/data_ingestion.py
+++ src/singledocument_chat/data_ingestion.py
@@ -4,17 +4,26 @@ import sys
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-
+from pytz import timezone
+import datetime
 from exception.custom_exception import DocumentPortalException
 from logger.custom_logger import CustomLogger
 from utils.model_loader import ModelLoader
 
 
 class SingleDocIngestion:
-    def __init__(self):
+    def __init__(self,data_dir:str="data/Single_document_chat", faiss_dir:str="faiss_index"):
         try:
             self.log = CustomLogger().get_logger(__name__)
+            
+            self.data_dir=Path(data_dir)
+            self.faiss_dir=Path(faiss_dir)
+            self.faiss_dir = Path(faiss_dir)
+            self.faiss_dir.mkdir(parents=True, exist_ok=True)
+
             self.loader = ModelLoader()
+            self.log.info("Initializing SingleDocument chat", temp_path=str(self.data_dir),faiss_path=str(self.faiss_dir))
+
             self.pdf_loader=PyPDFLoader()
             self.text_splitter = RecursiveCharacterTextSplitter()
             self.vector_store = FAISS()
@@ -23,16 +32,45 @@ class SingleDocIngestion:
             self.log.error(f"Error occurred during SingleDocIngestion initialization: {e}")
             raise DocumentPortalException(e)
 
-    def ingest_files(self):
+    def ingest_files(self, uploaded_files):
         try:
-            pass
+            documents=[]
+
+            for uploaded_file in uploaded_files:
+                unique_filename=f"session_{datetime.now(timezone.utc).strftime('%Y%m%d%H%M%S')}_{uuid.uuid4().hex[:8]}.pdf"
+                temp_path=self.data_dir / unique_filename
+
+                with open(temp_path, "wb") as f:
+                    f.write(uploaded_file.read())
+
+                self.log.info(f"File {uploaded_file.name} saved as {temp_path}")
+                loader=PyPDFLoader(str(temp_path)) 
+                docs=loader.load()
+                documents.extend(docs)
+            self.log.info(f"PDF files Loaded {len(documents)} documents")
+            return self._create_retreiver(documents) 
+            
         except Exception as e:
             self.log.error(f"Error occurred during file ingestion: {e}")
             raise DocumentPortalException(e)
 
-    def _create_retreiver(self):
+    def _create_retreiver(self,documents):
         try:
-            pass
+            splitter = RecursiveCharacterTextSplitter(CHUNK_SIZE=1000, CHUNK_OVERLAP=100)
+            chunks = splitter.split_documents(documents)
+            self.log.info(f"Documents split into {len(chunks)} chunks")
+
+            embeddings=self.loader.load_embeddings()
+            vector_store= FAISS.from_documents(documents=chunks,embedding=embeddings)
+
+        #Save FAISS Index
+            vector_store.save_local(str(self.faiss_dir))
+            self.log.info(f"FAISS index saved at {self.faiss_dir}")
+
+        #Add retriver as_retreiver 
+            retriever = vector_store.as_retriever(search_type="similarity",search_kwargs={"k": 5})
+            self.log.info("Retriever created successfully")
+            return retriever    
         except Exception as e:
             self.log.error(f"Error occurred during retriever creation: {e}")
             raise DocumentPortalException(e)
---
+++ test_singledocchat.py
@@ -0,0 +1,46 @@
+import sys
+from pathlib import Path
+from langchain_community.vectorstores import FAISS
+from src.singledocument_chat.data_ingestion import SingleDocIngestion
+from src.singledocument_chat.retrieval import ConversationalRAG
+from utils.model_loader import ModelLoader
+
+FAISS_INDEX_PATH = Path("faiss_index")
+
+def test_conv_rag(pdf_path:str,question:str):
+    try:
+        model_loader=ModelLoader()
+        if FAISS_INDEX_PATH.exists():
+            print("FAISS index already exists. Loading existing index.")
+            embeddings=model_loader.load_embeddings()
+            vector_store=FAISS.load_local(folder_path=str(FAISS_INDEX_PATH), embeddings=embeddings,allow_dangerous_deserialization=True)
+            retriever=vector_store.as_retriever(search_type="similarity,search_kwargs={"k}:5)
+        else:
+            print("FAISS index not found. Ingesting PDF and creating INDEX")
+            with open(pdf_path, "rb") as f:
+                uploaded_file = [f]
+                ingestor=SingleDocIngestion()
+                retriever=ingestor.ingest_files(uploaded_file)
+        print("Running Conversational RAG....")     
+        session_id="test_conv_rag"
+        rag=ConversationalRAG(retriever=retriever, session_id=session_id)
+        response=rag.invoke(question)
+        print(f"\nQuestion:{question}\nAnswer:{response}")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+
+if __name__ == "__main__":
+    pdf_path = "C:\\LLMOpSProjects\\Document_Portal\\Data\\Single_document_chat\\MasteringRAG.pdf"
+    question = "What are the key chunking methods in RAG"
+    
+    #r"C:\\LLMOpSProjects\\Document_Portal\\Data\\Single_document_chat\\MasteringRAG.pdf"
+
+    question = "What are the key challenges with LLM?"
+
+    if not Path(pdf_path).exists():
+        print(f"PDF file does not exist: {pdf_path}")
+        sys.exit(1)
+
+    test_conv_rag(pdf_path, question)
